5c61d28d1ace5fa19d776ad4f813340646865669
Thomas Athey <tom.l.athey@gmail.com>
2021-12-13 14:08:26 -0500

    save

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from train_test.neuron_train_test import Train_Test_Process
 
 if __name__ == '__main__':
     train_test_process = Train_Test_Process()
-    train_test_process.train()
+    train_test_process.test()
